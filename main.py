2c80551b75c02961058e02cbac013442b53e6bc2
tiypay <valrp832@gmail.com>
2026-05-13 23:00:50 +0200

    Increase uvicorn timeout to 620s for large sorts

--- main.py
+++ main.py
@@ -340,4 +340,4 @@ def health():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=620)
